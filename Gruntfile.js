28d6b9320578e371a00aca384c2ba3737af13cf1
Supriya <supriya.deore12@gmail.com>
2017-05-16 21:38:48 -0700

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+
+/* 
+ 
+ 
+ * Student Info: Name=SupriyaDeore, ID=19299
+ 
+ * Subject: CS557C_HW1_SUMMER_2015
+ 
+ * Author: SUPRIYA DEORE
+ 
+ * Filename: Gruntfile.js
+ 
+ * Date and Time: Oct 19, 2016 1:54:18 PM
+ 
+ * Project Name: MyFirstWebSite
+ 
+ 
+ */
+
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+
+/* 
+ 
+ 
+ * Student Info: Name=SupriyaDeore, ID=19299
+ 
+ * Subject: CS557C_HW1_SUMMER_2015
+ 
+ * Author: SUPRIYA DEORE
+ 
+ * Filename: gulpfile.js
+ 
+ * Date and Time: Oct 19, 2016 1:54:19 PM
+ 
+ * Project Name: MyFirstWebSite
+ 
+ 
+ */
+
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
